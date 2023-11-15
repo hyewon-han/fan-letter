1f67069a0e6d4afeac6fda7e535971e7cc5847d2
hyewon-han <hw1635@hanmail.net>
2023-11-15 19:30:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,17 +7,16 @@ import { Context } from "./Context";
 import fakeData from "./fakeData.json";
 
 function App() {
-  // 상태 초기화 함수를 사용하여 localStorage에서 데이터 불러오기
+  // 상태 초기화 함수를 사용하여 localStorage에서 데이터 불러오기!
   const [data, setData] = useState(() => {
     const savedData = localStorage.getItem("comments");
     return savedData ? JSON.parse(savedData) : fakeData;
   });
 
-  // 데이터가 변경될 때마다 localStorage에 저장
+  // 데이터가 변경될 때마다 localStorage에 저장!
   useEffect(() => {
     localStorage.setItem("comments", JSON.stringify(data));
   }, [data]);
-
   return (
     <Context.Provider value={{ data, setData }}>
       <ThemeProvider theme={theme}>
