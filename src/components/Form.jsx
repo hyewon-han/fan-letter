165d9ffe658a5d12942793f291b50f925e1b9074
hyewon-han <hw1635@hanmail.net>
2023-11-16 19:12:14 +0900

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -17,16 +17,6 @@ function Form() {
     const selectedChar = selectRef.current.value;
     return selectedChar;
   };
-  const formatDate = () => {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
-    return `${year}.${month}.${day}  ${hours}:${minutes}:${seconds}`;
-  };
 
   const formattedDate = new Intl.DateTimeFormat("ko-KR", {
     dateStyle: "full",
